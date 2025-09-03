c858788a726953ebb893c4ab21ff6a0176db4dea
reynaldabnerr <reynald030685@gmail.com>
2025-09-03 19:50:37 +0800

    add robots and sitemap generation files

---
+++ app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://www.abner.my.id/sitemap.xml',
+  }
+}
