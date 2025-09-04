243972c9582c008c90c9b65f4f277168a224d1f2
reynaldabnerr <reynald030685@gmail.com>
2025-09-04 12:12:24 +0800

    refactor: remove old robots.ts and add new robots.txt file; update sitemap entries with change frequency and priority

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,8 +5,32 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: 'https://www.abner.my.id',
       lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1.0,
+    },
+    {
+      url: 'https://www.abner.my.id/#about',
+      lastModified: new Date(),
       changeFrequency: 'monthly',
-      priority: 1,
+      priority: 0.9,
+    },
+    {
+      url: 'https://www.abner.my.id/#projects',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://www.abner.my.id/#github',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.7,
+    },
+    {
+      url: 'https://www.abner.my.id/#contact',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.6,
     },
   ]
 }
